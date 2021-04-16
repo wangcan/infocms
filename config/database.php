ff96b6df60a460b1f5df8548cfcba7921f1aa4b9
acanhost <acanstudio@canliang.wang>
2021-04-16 20:05:22 +0800

    develop test

--- config/database.php
+++ config/database.php
@@ -43,6 +43,26 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'infocms' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_INFOCMS_HOST', '127.0.0.1'),
+            'port' => env('DB_INFOCMS_PORT', '3306'),
+            'database' => env('DB_INFOCMS_DATABASE', 'forge'),
+            'username' => env('DB_INFOCMS_USERNAME', 'forge'),
+            'password' => env('DB_INFOCMS_PASSWORD', ''),
+            'unix_socket' => env('DB_INFOCMS_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => env('DB_INFOCMS_PREFIX', ''),
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
