7a908585d5efa7bc97f4bc2268ae08deb5dee317
acanhost <acanstudio@canliang.wang>
2021-04-16 16:39:45 +0800

    develop test

--- library/src/Helpers/Operation.php
+++ library/src/Helpers/Operation.php
@@ -61,6 +61,19 @@ class Helper
 
         return view('newsletters.unsubscribed');
     }
+    public function authenticate(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            $user = User::where('email', $request->input('email'))->first();
+
+            return (new UserResource($user))
+                    ->additional(['meta' => [
+                        'access_token' => $user->api_token
+                    ]]);
+        }
+
+        return response()->json(['message' => 'This action is unauthorized.'], 401);
+    }
     public function index()
     {
 //use Illuminate\Support\Facades\Http;
@@ -158,5 +171,211 @@ class UserTokenController extends Controller
         return PostResource::collection(
             $user->posts()->latest()->paginate($request->input('limit', 20))
         );
+    }
+    public function store(CommentsRequest $request, Post $post): CommentResource
+    {
+        $comment = new CommentResource(
+            Auth::user()->comments()->create([
+                'post_id' => $post->id,
+                'content' => $request->input('content')
+            ])
+        );
+
+        broadcast(new CommentPosted($comment, $post))->toOthers();
+
+        return $comment;
+    }
+}
+
+
+<?php
+
+namespace Yeelight\Http\Controllers\Api\Controllers;
+
+use Dingo\Api\Exception\DeleteResourceFailedException;
+use Yeelight\Http\Requests\Api\ProductModelCreateRequest;
+use Yeelight\Http\Requests\Api\ProductModelUpdateRequest;
+use Yeelight\Repositories\Interfaces\ProductModelRepository;
+use Yeelight\Validators\ProductModelValidator;
+
+/**
+ * Class ProductModelsController
+ *
+ * @category Yeelight
+ *
+ * @package Yeelight\Http\Controllers\Api\Controllers
+ *
+ * @author Sheldon Lee <xdlee110@gmail.com>
+ *
+ * @license https://opensource.org/licenses/MIT MIT
+ *
+ * @link https://www.yeelight.com
+ */
+class ProductModelsController extends BaseController
+{
+    /**
+     * ProductModelRepository
+     *
+     * @var ProductModelRepository
+     */
+    protected $repository;
+
+    /**
+     * ProductModelValidator
+     *
+     * @var ProductModelValidator
+     */
+    protected $validator;
+
+    /**
+     * ProductModelsController constructor.
+     *
+     * @param ProductModelRepository $repository ProductModelRepository
+     * @param ProductModelValidator $validator ProductModelValidator
+     */
+    public function __construct(
+        ProductModelRepository $repository,
+        ProductModelValidator $validator
+    ) {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param ProductModelCreateRequest $request ProductModelCreateRequest
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductModelCreateRequest $request)
+    {
+        $data = $request->all();
+
+        $productModel = $this->repository->create($data);
+
+        // throw exception if store failed
+        // throw new StoreResourceFailedException('Failed to store.');
+
+        // A. return 201 created
+        // return $this->response->created(null);
+
+        // B. return data
+        return $this->response->created();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param ProductModelUpdateRequest $request ProductModelUpdateRequest
+     * @param string $id id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductModelUpdateRequest $request, $id)
+    {
+        $data = $request->all();
+
+        $productModel = $this->repository->update($data, $id);
+
+        // throw exception if update failed
+        // throw new UpdateResourceFailedException('Failed to update.');
+
+        // Updated, return 204 No Content
+        return $this->response->noContent();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $deleted = $this->repository->delete($id);
+
+        if ($deleted) {
+            // Deleted, return 204 No Content
+            return $this->response->noContent();
+        } else {
+            // Failed, throw exception
+            throw new DeleteResourceFailedException('Failed to delete.');
+        }
+    }
+}
+
+
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UsersRequest;
+use App\Http\Resources\User as UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Return the users.
+     */
+    public function index(Request $request): ResourceCollection
+    {
+        return UserResource::collection(
+            User::withCount(['comments', 'posts'])->with('roles')->latest()->paginate($request->input('limit', 20))
+        );
+    }
+
+    /**
+     * Return the specified resource.
+     */
+    public function show(User $user): UserResource
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UsersRequest $request, User $user): UserResource
+    {
+        $this->authorize('update', $user);
+
+        if ($request->filled('password')) {
+            $request->merge([
+                'password' => Hash::make($request->input('password'))
+            ]);
+        }
+
+        $user->update(array_filter($request->only(['name', 'email', 'password'])));
+
+        return new UserResource($user);
     }
 }
